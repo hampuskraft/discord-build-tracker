6f4cdd1b43edd477ebfcf9f5fdc8f746b94b501e
Hampus Kraft <hello@hampuskraft.com>
2024-05-11 21:32:51 +0200

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
-DROP TABLE IF EXISTS builds;
 CREATE TABLE builds (
     channel TEXT NOT NULL,
     build_number INTEGER NOT NULL,
     version_hash TEXT NOT NULL,
     timestamp INTEGER NOT NULL,
     rollback INTEGER NOT NULL,
-    PRIMARY KEY (channel, build_number, version_hash, timestamp)
+    PRIMARY KEY (channel, timestamp, build_number, version_hash)
 );
+
+CREATE INDEX IF NOT EXISTS idx_channel_rollback ON builds (channel, rollback);
